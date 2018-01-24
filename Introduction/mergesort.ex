e08cb06c727e210acc7ba858136ba47be64b4747
Unknown <blendaf@kth.se>
2018-01-24 12:45:08 +0100

    mergesort done

--- Introduction/mergesort.ex
+++ Introduction/mergesort.ex
@@ -1,1 +1,27 @@
 defmodule MergeSort do
+
+    
+    def split([], left, right), do: {left, right}
+    def split([head|tail], left, right) do
+        split(tail, [head|right], left)
+    end 
+
+    def merge([],right) do right end 
+    def merge(left, []) do left end  
+    def merge([hL|tL], [hR|_tR] = right) when hL < hR do 
+        [hL|merge(tL,right)]
+    end
+    def merge([_hL|_tL] = left, [hR|tR]) do 
+        [hR|merge(tR, left)]
+    end
+
+    def mergesort([]) do [] end 
+    def mergesort([element]), do: [element]
+    def mergesort(l) do
+        {left, right} = split(l, [], [])
+        merge(mergesort(left), mergesort(right))
+    end 
+   
+end
+
+IO.puts inspect(MergeSort.mergesort([1, 3, 6, 2, 4, 6]))
